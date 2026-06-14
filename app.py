6f3e0aac446de21154977d4b6250552559896c76
Debasish Kumar <dhirendraprasaddav@gmail.com>
2026-06-14 11:57:57 +0530

    Increase cache and timeout

--- app.py
+++ app.py
@@ -63,10 +63,10 @@ def goto_details(tmdb_id: int):
 # =============================
 # API HELPERS
 # =============================
-@st.cache_data(ttl=30)  # short cache for autocomplete
+@st.cache_data(ttl=600)  # short cache for autocomplete
 def api_get_json(path: str, params: dict | None = None):
     try:
-        r = requests.get(f"{API_BASE}{path}", params=params, timeout=25)
+        r = requests.get(f"{API_BASE}{path}",params=params,timeout=60)
         if r.status_code >= 400:
             return None, f"HTTP {r.status_code}: {r.text[:300]}"
         return r.json(), None
